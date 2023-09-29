791200b66fad253692faae686ed961b456d3301c
Patrick Dias Siqueira <12114788@aluno.newtonpaiva.br>
2023-09-29 20:23:50 -0300

    commit

---
+++ pratica3/Questao1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Questao1 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner();
+        
+        System.out.println("Hello world!");
+    }
+}
